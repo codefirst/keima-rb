982da08df71c1dc8e0812af342ef76f9259c0d13
suer <suetsugu.r@gmail.com>
2012-09-01 02:34:44 +0900

    fixed: test failure

--- spec/keima/client_spec.rb
+++ spec/keima/client_spec.rb
@@ -5,9 +5,5 @@ describe Keima::Client do
     before { @client = Keima::Client.new('http://localhost:3001', '503b7d29e1f85c9632000001') }
     subject { @client }
     its(:publish_url) { should == 'http://localhost:3001/publish/503b7d29e1f85c9632000001' }
-    context 'data to json ' do
-      subject { @client.to_json({:name => 'mzp', :message => 'Hello!'}) }
-      it { should == '{"name":"mzp","message":"Hello!"}' }
-    end
   end
 end
